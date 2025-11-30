fad9002585931354c1d424ddeb86a7d0a45a9bb5
provi2025 <sven_ht@web.de>
2025-11-30 16:50:58 +0100

    kk

---
+++ random-number/script.js
@@ -0,0 +1,27 @@
+const a = document.getElementById("button1")
+const b = document.getElementById("label1")
+let generatedNum
+const min = 1
+const max = 100
+a.onclick = function(){
+  generatedNum = Math.floor(Math.random() * max) + min
+  b.textContent = generatedNum
+  console.log(generatedNum)
+}
+const c = document.getElementById("button3")
+const d = document.getElementById("label2")
+c.onclick = function(){
+  generatedNum = Math.random() * max + min
+  d.textContent = generatedNum
+  console.log(generatedNum)
+}
+const e = document.getElementById("button2")
+const f = document.getElementById("button4")
+e.onclick = function(){
+  generatedNum = 0
+  b.textContent = generatedNum
+}
+f.onclick = function(){
+  generatedNum = 0
+  d.textContent = generatedNum
+}
--- random-number/src/App.js
+++
@@ -1,7 +0,0 @@
-import RandomNumberGenerator from './components/RandomNumberGenerator'
-
-import './App.css'
-
-const App = () => <RandomNumberGenerator />
-
-export default App
