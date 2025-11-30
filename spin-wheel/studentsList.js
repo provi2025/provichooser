7443bac0576ceca834e5951e5b7dde23b88414fc
provi2025 <sven_ht@web.de>
2025-11-30 15:54:30 +0100

    push

--- spin-wheel/studentsList.js
+++ spin-wheel/studentsList.js
@@ -1,5 +1,5 @@
 const studentsList = [
-    "John", "Jill", "Jane", "Tom", "Alice", "Bob", "Charlie", "Eve", "Mike",
-    "Lucy", "David", "Emma", "James", "Mia", "Jack", "Sophia", "Liam",
-    "Olivia", "Ben", "Chloe", "Henry", "Grace"
+    "Schnoch", "Toni", "La Haut", "James", "Simme", "Massa", "Hillus", "Marv", "Tom",
+    "Jonny", "El Lecko", "Henne", "Ferdinand", "Christian", "Werner", "Jan", "Michael",
+    "Ulf", "Bernd", "Dieter", "Uwe", "Dirk"
 ];
